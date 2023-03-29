55d564c6df8c698c7d630385adfb414409b0a08b
annisaamelia <nisamelptr@gmail.com>
2023-03-30 05:01:39 +0800

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\Petugas::create([
+            'nama_petugas' => 'Administrator',
+            'username' => 'admin',
+            'password' => Hash::make('password'),
+            'telp' => '082134435665',
+            'level' => 'admin',
+        ]);
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
